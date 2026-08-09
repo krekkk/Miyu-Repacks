1eb3cde9e3c8f36efecfa7e95bb1cfa88c8682a1
krekkk <afifudin18051979@gmail.com>
2026-08-09 18:39:13 +0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,16 @@
+{
+    title: "Max Payne 2 Miyu Repacks",
+    category: "pc",
+    platform: "PC",
+    os: "Windows XP ke atas",
+    image: "max-payne-2.jpg",
+    description: "Max Payne 2 Miyu Repacks.",
+    tutorial: [
+        "Ekstrak Max Payne 2 Miyu Repacks.",
+        "Jalankan setup.exe.",
+        "Tunggu proses instalasi sampai selesai.",
+        "Jalankan game.",
+        "Selamat bermain!"
+    ],
+    download: "https://drive.google.com/file/d/1fKl9jQeSYIcSqbKZDXokDC7sI2L09rzw/view?usp=sharing"
+}
